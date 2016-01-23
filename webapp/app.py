d6c31dba5294deb389a4f25a3200cb6be9feed23
HyeonRok Lee <sealab2012@gmail.com>
2016-01-23 20:01:26 +0900

    flask webapp

---
+++ webapp/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from quantom import *
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data = generate_stock_prices(n=100)
+    return render_template('index.html', data=data)
+
+if __name__ == "__main__":
+    app.run(debug = True,port=8080, passthrough_errors=True)
