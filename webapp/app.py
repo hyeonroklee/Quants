7731b964d92bb4a6594059c0fcc9caf545f52ecb
HyeonRok Lee <sealab2012@gmail.com>
2016-02-01 00:53:44 +0900

    add d3 charting

--- webapp/app.py
+++ webapp/app.py
@@ -6,8 +6,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data = generate_stock_prices(n=100)
+    data = generate_stock_prices(n=30)
     return render_template('index.html', data=data)
+
+@app.route('/candlestick')
+def candlestic():
+    return render_template('candlestick.html')
+
+@app.route('/test')
+def test():
+    title = 'this is a Title'
+    return render_template('test.html',t=title)
+
+
 
 if __name__ == "__main__":
     app.run(debug = True,port=8080, passthrough_errors=True)
---
+++ webapp/static/js/require.config.js
@@ -0,0 +1,17 @@
+/* jshint unused:false */
+/* global require: true */
+
+var require = {
+    paths: {
+        'd3': 'lib/d3',
+        'jstat': 'lib/jstat',
+        'moment': 'lib/moment',
+        'moment-range': 'lib/moment-range',
+        'modernizr': 'lib/modernizr'
+    },
+    shim: {
+        'modernizr': {
+            exports: 'Modernizr'
+        }
+    }
+};
